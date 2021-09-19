fa98bf72dd7136415420b5a64e17a778a6caf546
07ENT <73337792+07ENT@users.noreply.github.com>
2021-09-19 23:41:51 +0800

    Add files via upload

---
+++ 03219000147/3219000147/SE_individual_work-master/3219000147/thesisDefenseUnitTest/thesisDefenseUnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace thesisDefenseUnitTest
+{
+	TEST_CLASS(thesisDefenseUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
